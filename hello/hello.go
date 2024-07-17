884ad254b5e1f48b7fb73a62c1b4513d61750f45
Gennady Oleshko <gennady@oleshko-g.ru>
2024-07-17 21:00:20 +0300

    Return greetings for multiple people

--- hello/hello.go
+++ hello/hello.go
@@ -9,19 +9,22 @@ import (
 
 func main() {
 	// Set properties of the predefined Logger, including
-	// the log entry prefix and a flag to siable printing
+	// the log entry prefix and a flag to disable printing
 	// the time, source file, and line number.
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	// Request a greeting message.
-	message, err := greetings.Hello("Gena")
+	// A slice of names.
+	names := []string{"Gena", "Notempty", "Valera"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
 		log.Fatal(err)
 	}
-	// If no error was returned, print the returned message
-	// to the console.
-	fmt.Println(message)
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
 }
